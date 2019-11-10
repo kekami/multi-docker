784ad1066ef2f2a0f0c71da2231d1318cbcf2952
Kevin Kamimura <kevin.kamimura@icloud.com>
2019-11-10 21:18:16 +0100

    re-added client

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Fib from "./Fib";
+import OtherPage from "./OtherPage";
+
+function App() {
+  return (
+      <Router>
+        <div className="App">
+            <h1>Welcome to React</h1>
+            <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherPage" component={OtherPage} />
+            </div>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
